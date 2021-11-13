9a2c779e14aca63033efc15aee0e0a04e7fc295a
caioraduy <79532195+caioraduy@users.noreply.github.com>
2021-11-13 15:56:40 -0300

    Update programa_pibic_com_def.py

--- programa_pibic_com_def.py
+++ programa_pibic_com_def.py
@@ -2,7 +2,8 @@ import pm4py
 import pandas as pd
 import os
 
-
+# This is one of the most important function of the program, it utlizes the method in the article for
+# calculating the F-score
 def calcula_f_score(list_drifts_detectados, tamanho_janela, numero_de_drifts_reais_da_base, drift_reias):
     dist_max = int(tamanho_janela)
     numero_drifts_reais = numero_de_drifts_reais_da_base
@@ -11,6 +12,9 @@ def calcula_f_score(list_drifts_detectados, tamanho_janela, numero_de_drifts_rea
         drifts_reais = [500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500]
     elif numero_de_drifts_reais_da_base == 1:
         drifts_reais = [500]
+    else:
+        drifts_reais= drift_reias
+
 
     list_drifts_detectados = list_drifts_detectados
 
@@ -39,7 +43,7 @@ def calcula_f_score(list_drifts_detectados, tamanho_janela, numero_de_drifts_rea
 
     return f_score
 
-
+#This function calculates real drifts of the base with 5000 trace and 9 drifts
 def calcula_drift_reais_analise_events_5k(log, event_stream):
     lista_com_nome_do_trace = []
     for i in range(500, 5000, 500):
@@ -53,7 +57,8 @@ def calcula_drift_reais_analise_events_5k(log, event_stream):
                 break
     return lista_drifts_reais_events
 
-
+#This function calculates real drifts of the base with 1000 trace and 1 drifts, when reading
+# the file as an event stream
 def calcula_drift_reais_analise_events_sudden_1000(log, event_stream):
     drift_real = log[500].attributes['concept:name']
     for i, item in enumerate(event_stream):
@@ -62,7 +67,7 @@ def calcula_drift_reais_analise_events_sudden_1000(log, event_stream):
             break
     return drift_real_event
 
-
+#this function extracts the information of the file with the output of ProDrift Plugin
 def extrai_informacao_do_arquivo_base1drift_apromore(nome_do_arquivo):
     nome_do_arquivo_analisado_na_funcao = nome_do_arquivo
     splitline = nome_do_arquivo_analisado_na_funcao.split('_')
@@ -75,11 +80,11 @@ def extrai_informacao_do_arquivo_base1drift_apromore(nome_do_arquivo):
     if tamanho_da_janela == 'default':
         tamanho_da_janela = 200
     else:
-        tamanho_da_janela = int(tamanho_da_janela)
+        tamanho_da_janela = int(float(tamanho_da_janela))
 
     return tool, nome_log, abordagem, tipo_janelamento, tamanho_da_janela
 
-
+#this function extracts information from the archive of the bases with 1000 and 1 drift a VDD
 def extrai_informacao_do_arquivo_base1drift_vdd(nome_do_arquivo):
     nome_do_arquivo_analisado_na_funcao = nome_do_arquivo
     splitline = nome_do_arquivo_analisado_na_funcao.split('_')
@@ -95,7 +100,7 @@ def extrai_informacao_do_arquivo_base1drift_vdd(nome_do_arquivo):
 
     return tool, nome_log, abordagem, tipo_janelamento, tamanho_da_janela, win_step
 
-
+#this funcion extracts information of the archives .txt for the event logs with 9 drift and 5000 traces for Apromore
 def extrai_informacao_do_arquivo_base9drift_apromore(nome_do_arquivo):
     nome_do_arquivo_analisado_na_funcao = nome_do_arquivo
     splitline = nome_do_arquivo_analisado_na_funcao.split('_')
@@ -110,13 +115,25 @@ def extrai_informacao_do_arquivo_base9drift_apromore(nome_do_arquivo):
     tipo_janelamento = splitline[4]
     tool = splitline[5]
     tool = tool[:-4]
+
     return tool, nome_log, abordagem, tipo_janelamento, tamanho_da_janela
-
-
+#this funcion extracts information of the archives .txt for the event logs with 9 drift and 5000 traces for VDD
+def extrai_informacao_do_arquivo_base9drift_vdd(nome_do_arquivo):
+    nome_do_arquivo_analisado_na_funcao = nome_do_arquivo
+    splitline = nome_do_arquivo_analisado_na_funcao.split('_')
+    nome_log = splitline[0]
+    abordagem = 'trace'
+    tamanho_da_janela = splitline[2]
+    tamanho_da_janela = int(tamanho_da_janela[4:])
+    tipo_janelamento = 'fwin'
+    tool = 'vdd'
+    win_step=splitline[3]
+    win_step=int(win_step[5:])
+    return tool, nome_log, abordagem, tipo_janelamento, tamanho_da_janela, win_step
 dicionario_com_eventos_5k = {}
 dicionario_com_eventos_1000 = {}
 
-
+#this fucntion reads the event log as a event stream and find the real drifts
 def acha_drifts_reais_no_xes(caminho_logs):
     for raiz, diretorios, arquivos in os.walk(caminho_logs):
         for arquivo in arquivos:
@@ -137,7 +154,7 @@ def acha_drifts_reais_no_xes(caminho_logs):
                 drifts_reais = calcula_drift_reais_analise_events_sudden_1000(log, event_stream)
                 dicionario_com_eventos_1000[arquivo_para_dic] = drifts_reais
 
-
+#this function finds the real drift for the event logs with 5000 traces
 def acha_drifts_reais_5k(abordagem, arquivo):
     drifts_reais_5k = []
     if abordagem == 'trace':
@@ -150,7 +167,7 @@ def acha_drifts_reais_5k(abordagem, arquivo):
             drifts_reais_5k.append(x)
         return drifts_reais_5k
 
-
+#this function finds the real drift for the event logs with 1000 traces
 def acha_drifts_reais_sudden_1000(abordagem, arquivo):
     lista_drifts_reais = []
     if abordagem == 'trace':
@@ -166,7 +183,7 @@ def acha_drifts_reais_sudden_1000(abordagem, arquivo):
 
 df = pd.DataFrame()
 
-
+#this function add the data to a pandas dataframe that will be the output of the method
 def adiciona_para_dataframe(tool, nome_log, abordagem, tipo_janelamento, tamanho_da_janela,
                             f_score):
     linha = [[tool, nome_log, abordagem, tipo_janelamento, tamanho_da_janela,
@@ -183,12 +200,14 @@ def adiciona_para_dataframe(tool, nome_log, abordagem, tipo_janelamento, tamanho
     global df
     df = df.append(df2, ignore_index=True)
 
-
-def acha_os_drifts_detectados_calcula_f_score_adiciona_no_df_vdd(f, abordagem, tamanho_da_janela, tool, nome_log,
-                                                                 tipo_janelamento):
+#this function finds the drifts detected, calculate the F-score and add the F-score and other information to the dataframe
+def acha_os_drifts_detectados_calcula_f_score_adiciona_no_df_vdd(f, abordagem, tamanho_da_janela,drifts_reais,
+                                                                 tool, nome_log,
+                                                                 tipo_janelamento,win_step):
     procura_console_vdd = 'x lines:'
     drift = []
     found = False
+    lista_drifts_reais= drifts_reais
     drift_por_cluster = []
     for line in f:
         if found:
@@ -196,6 +215,7 @@ def acha_os_drifts_detectados_calcula_f_score_adiciona_no_df_vdd(f, abordagem, t
             s2 = s1.replace(']', '')
             s3 = s2.replace('\n', '')
             splitline = s3.split(',')
+
             drift_por_cluster = [int(x) for x in splitline]
             lista_de_drifts_por_traces_vdd = []
             for x in drift_por_cluster:
@@ -209,7 +229,7 @@ def acha_os_drifts_detectados_calcula_f_score_adiciona_no_df_vdd(f, abordagem, t
         if procura_console_vdd in line:
             found = True
 
-
+# this function finds the detected drifts and calculate de f-score for the toll Apromore ProDrift plugin
 def acha_os_drifts_detectados_calcula_f_score_adiciona_no_df_apromore(f, abordagem, tamanho_da_janela, drifts_reais,
                                                                       tool, nome_log, tipo_janelamento):
     drift = []
@@ -228,19 +248,28 @@ def acha_os_drifts_detectados_calcula_f_score_adiciona_no_df_apromore(f, abordag
     adiciona_para_dataframe(tool, nome_log, abordagem, tipo_janelamento, tamanho_da_janela,
                             f_score)
 
-
+# this function reads the output from te frameworks and calls the function for F-score calcule
 def le_output_dos_frameworks_e_calcula_f_score(caminho_procura):
     for raiz, diretorios, arquivos in os.walk(caminho_procura):
         # for raiz, diretorios, arquivos in os.walk(caminho_procura_5k_apromore):
         for arquivo in arquivos:
             caminho_completo = os.path.join(raiz, arquivo)
             if '5k' in arquivo:
-                tool, nome_log, abordagem, tipo_janelamento, tamanho_da_janela \
+                if 'apromore' in arquivo:
+                    tool, nome_log, abordagem, tipo_janelamento, tamanho_da_janela \
                     = extrai_informacao_do_arquivo_base9drift_apromore(arquivo)
+                elif 'vdd' in arquivo:
+                    tool, nome_log, abordagem, tipo_janelamento, tamanho_da_janela, win_step \
+                        =extrai_informacao_do_arquivo_base9drift_vdd(arquivo)
                 drifts_reais = acha_drifts_reais_5k(abordagem, arquivo)
             elif '1000' in arquivo:
-                tool, nome_log, abordagem, tipo_janelamento, tamanho_da_janela \
+                if 'apromore' in arquivo:
+                    tool, nome_log, abordagem, tipo_janelamento, tamanho_da_janela \
                     = extrai_informacao_do_arquivo_base1drift_apromore(arquivo)
+                elif 'vdd' in arquivo:
+                    tool, nome_log, abordagem, tipo_janelamento, tamanho_da_janela, win_step \
+                        = extrai_informacao_do_arquivo_base1drift_vdd(arquivo)
+
                 drifts_reais = acha_drifts_reais_sudden_1000(abordagem, arquivo)
             with open(caminho_completo, 'r', errors='ignore') as f:
                 if 'apromore' in arquivo:
@@ -250,29 +279,38 @@ def le_output_dos_frameworks_e_calcula_f_score(caminho_procura):
                 elif 'vdd' in arquivo:
                     acha_os_drifts_detectados_calcula_f_score_adiciona_no_df_vdd(f, abordagem, tamanho_da_janela,
                                                                                  drifts_reais, tool,
-                                                                                 nome_log, tipo_janelamento)
+                                                                                 nome_log, tipo_janelamento,win_step)
 
 
 if __name__ == '__main__':
     df = pd.DataFrame()
-    # use o mesmo nome de variável e comente o que não está utilizando, facilita pois não precisa
-    # procurar onde a variável é utilizada para alterar
-    caminho_procura_5k_apromore = 'D:/PIBIC/arquivos_analise_programa/trace_9_drift_apromore/'
+    
+    #here, we put the path to the files
+    # the caminho_vdd_sudden_1000 and data/VDD_output_1000 has to be filled with .txt filles that are bigger than 100MB, they have to be downloaded at Kaggle and inputted at the 
+    #respective directory 'data/VDD_output_1000' and 'data/VDD_output_5k'
+    caminho_vdd_sudden_5k
+    caminho_procura_5k_apromore = 'data/output_apromore_5k/'
     caminho_procura_5k_apromore_teste = 'data/output_apromore_5k'
-    caminho_procura_sudden_1000_apromore = 'D:/PIBIC/arquivos_analise_programa/trace_1_drift_apromore/'
+    caminho_procura_sudden_1000_apromore = 'data/output_apromore/'
     caminho_procura_sudden_1000_apromore_teste = 'data/output_apromore'
-    caminho_vdd_sudden_1000 = 'D:/PIBIC/Console_vdd_drift_all/vdd_drift_all_sudden_1000'
+    caminho_vdd_sudden_1000 = 'data/VDD_output_1000'
+    caminho_vdd_sudden_5k='data/VDD_output_5k'
     procura_console_vdd = 'x lines:'
-    # aqui eu estou lendo os drift reais para as bases de 5k
+  
     caminho_logs_5k = 'data/data_5k/'
     caminho_logs_1000 = 'data/data_1000'
     caminho_logs_5k_teste = 'data/data_5k_teste'
     caminho_logs_1000_teste = 'data/data_teste_sudden'
-    # aqui estou chamando a função acha os drifts reais nolog e cria um dicionário para 5k e sudden 1000
-    acha_drifts_reais_no_xes(caminho_logs_5k_teste)
-    acha_drifts_reais_no_xes(caminho_logs_1000_teste)
-    le_output_dos_frameworks_e_calcula_f_score(caminho_procura_5k_apromore_teste)
-    le_output_dos_frameworks_e_calcula_f_score(caminho_procura_sudden_1000_apromore_teste)
+    
+    acha_drifts_reais_no_xes(caminho_logs_5k)
+    acha_drifts_reais_no_xes(caminho_logs_1000)
+    le_output_dos_frameworks_e_calcula_f_score(caminho_procura_5k_apromore)
+    le_output_dos_frameworks_e_calcula_f_score(caminho_procura_sudden_1000_apromore)
+    le_output_dos_frameworks_e_calcula_f_score(caminho_vdd_sudden_1000)
+    le_output_dos_frameworks_e_calcula_f_score(caminho_vdd_sudden_5k)
+
+
+
     print(df)
-    # utilize caminho relativo, sugiro criar uma pasta output dentro de data
-    df.to_excel('D:\\PIBIC\\resultados_pibic\\resultados_logs_ddm_runs.xlsx', index=False)
+    
+    df.to_excel('Data\\OutputData\\F_Score_Results.xlsx', index=False)
